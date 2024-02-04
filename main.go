1779d500a6a3a539372864a139beb603445be93f
WonYus <lucy_oliv@outlook.com>
2024-02-04 23:35:22 +0700

    update

--- main.go
+++ main.go
@@ -1,41 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"github.com/wonyus/update-status-service/controllers"
 	"github.com/wonyus/update-status-service/initials"
-
-	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
 )
 
-var ginLambda *chiadapter.ChiLambda
-
 func init() {
-	// stdout and stderr are sent to AWS CloudWatch Logs
 	initials.InitDB()
-	r := initials.InitHttpServer()
 	client := initials.InitialMqttClient(controllers.MessagePubHandler)
 	controllers.DefaultSubscribeHandler(client)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("healty")
-		w.Write([]byte("healty"))
-	})
-
-	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("ping")
-		w.Write([]byte("pong"))
-	})
-	r.Post("/update-status", controllers.UpdateStatus)
-
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	route := gin.Default()
+	route.Use(gin.Logger())
+	route.Use(gin.Recovery())
+
+	route.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "online", "message": "Hello, world!"})
+	})
+	route.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": 200, "message": "pong"})
+	})
+	route.Run(":80")
 }
